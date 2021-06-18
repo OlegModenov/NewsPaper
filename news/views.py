29dad428f6b743de03e59c72a7f34ca6b8386ed1
Oleg Modenov <olegmodenov@gmail.com>
2021-06-18 20:14:01 +0500

    Настроены flatpages

--- news/views.py
+++ news/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['time_now'] = datetime.utcnow()
+    #     context['some_variable'] = None
+    #     return context
+
+
+class NewsDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'news_one.html'  # название шаблона будет product.html
+    context_object_name = 'news_one'
